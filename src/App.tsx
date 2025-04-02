623a403848ad4f3a7a9601245d4f3de4e67ff0e2
atcherdsd <tchernak.csg@gmail.com>
2025-04-02 23:14:12 +0300

    init: set up vite

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <div className="flex items-start justify-start">
+                <h1 className="text-4xl font-bold text-green-500">Calculator</h1>
+            </div>
+        </>
+    )
+}
+
+export default App;
